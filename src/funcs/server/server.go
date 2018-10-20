67cc7480d190581f95e987c4740801bfddd8af44
Martin Angers <martin.n.angers@gmail.com>
2018-10-20 15:25:02 -0400

    add server

---
+++ src/funcs/server/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       os.Getenv("NETLIFY_TEST_GREET"),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
