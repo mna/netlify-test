862963d5d62f041a3fa0c0348bf8501f56dc05d9
Martin Angers <martin.n.angers@gmail.com>
2018-10-20 15:56:17 -0400

    print everything out

--- src/funcs/server/server.go
+++ src/funcs/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
@@ -10,15 +11,28 @@ import (
 )
 
 func handler(ctx context.Context, r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	if r.RequestContext.ResourcePath == "/fail" {
+	path := r.QueryStringParameters["path"]
+	if path == "fail" {
 		return &events.APIGatewayProxyResponse{
 			StatusCode: 400,
 			Body:       "Fail!",
 		}, nil
 	}
+	var body bytes.Buffer
+	for k, v := range r.Headers {
+		fmt.Fprintf(&body, "%s = %v\n", k, v)
+	}
+	fmt.Fprintf(&body, "Method = %s\n", r.HTTPMethod)
+	fmt.Fprintf(&body, "Path = %s\n", r.Path)
+	fmt.Fprintf(&body, "PathParameters = %v\n", r.PathParameters)
+	fmt.Fprintf(&body, "QueryStringParameters = %v\n", r.QueryStringParameters)
+	fmt.Fprintf(&body, "Resource = %s\n", r.Resource)
+	fmt.Fprintf(&body, "StageVariables = %v\n", r.StageVariables)
+	fmt.Fprintf(&body, "NETLIFY_TEST_GREET = %s\n", os.Getenv("NETLIFY_TEST_GREET"))
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("%v", r.QueryStringParameters) + "\n" + r.Resource + "\n" + r.RequestContext.ResourcePath + "\n" + r.Path + "\n" + os.Getenv("NETLIFY_TEST_GREET"),
+		Body:       body.String(),
 	}, nil
 }
 
