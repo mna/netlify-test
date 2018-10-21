75b2c75630f45c14d10b5e7a2f34777543f130f4
Martin Angers <martin.n.angers@gmail.com>
2018-10-21 14:15:37 -0400

    log more details

--- src/funcs/server/server.go
+++ src/funcs/server/server.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/kr/pretty"
 )
 
 const funcPrefix = "/.netlify/functions/server"
@@ -22,6 +23,7 @@ func handler(ctx context.Context, r events.APIGatewayProxyRequest) (*events.APIG
 		}, nil
 	}
 	var body bytes.Buffer
+	fmt.Fprintln(&body, "APIGatewayProxyRequest:")
 	for k, v := range r.Headers {
 		fmt.Fprintf(&body, "%s = %v\n", k, v)
 	}
@@ -32,6 +34,19 @@ func handler(ctx context.Context, r events.APIGatewayProxyRequest) (*events.APIG
 	fmt.Fprintf(&body, "Resource = %s\n", r.Resource)
 	fmt.Fprintf(&body, "StageVariables = %v\n", r.StageVariables)
 	fmt.Fprintf(&body, "NETLIFY_TEST_GREET = %s\n", os.Getenv("NETLIFY_TEST_GREET"))
+
+	fmt.Fprintln(&body, "\nRequestContext:")
+	fmt.Fprintf(&body, "APIID = %s\n", r.RequestContext.APIID)
+	fmt.Fprintf(&body, "AccountID = %s\n", r.RequestContext.AccountID)
+	fmt.Fprintf(&body, "Authorizer = %v\n", r.RequestContext.Authorizer)
+	fmt.Fprintf(&body, "HTTPMethod = %s\n", r.RequestContext.HTTPMethod)
+	fmt.Fprintf(&body, "RequestID = %s\n", r.RequestContext.RequestID)
+	fmt.Fprintf(&body, "ResourceID = %s\n", r.RequestContext.ResourceID)
+	fmt.Fprintf(&body, "ResourcePath = %s\n", r.RequestContext.ResourcePath)
+	fmt.Fprintf(&body, "Stage = %s\n", r.RequestContext.Stage)
+
+	fmt.Fprintln(&body, "\nIdentity:")
+	pretty.Fprintf(&body, "%#v\n", r.RequestContext.Identity)
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
